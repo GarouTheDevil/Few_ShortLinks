61e3b30658ddb8ad00ab7ae5d6b673bbe638e9cc
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-29 20:57:21 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 from os import environ
 
 class Config(object):
- API_ID = environ.get("API_ID", 10293305)
- API_HASH = environ.get("API_HASH", "9b3bb6ac48fbbc1195e4657fe30858e9")
- BOT_TOKEN = environ.get("BOT_TOKEN", "5519560955:AAHkEKKpNMIoptfSbk9vV5q-Wh5XTOmg8n4")
- BITLY_KEY = environ.get("96bbcd78781ecd9018670be6c876452af049084a")
- OWNER = environ.get("OWNER", "Stay007")
- USERS = [int(i) for i in environ.get("USERS", "1802569102 1016768333").split(" ")]
+ API_ID = int(environ.get("API_ID")
+ API_HASH = int(environ.get("API_HASH", ""))
+ BOT_TOKEN = int(environ.get("BOT_TOKEN", ""))
+ BITLY_KEY = int(environ.get("BITLY_KEY, """))
+ OWNER = int(environ.get("OWNER", ""))
+ USERS = [int(i) for i in environ.get("USERS", "").split(" ")]
