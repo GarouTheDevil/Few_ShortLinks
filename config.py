71d21552eb5e6e869d8b7331ac8a1e47553bf3ac
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-29 17:54:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 BITLY_KEY = environ.get('BITLY_KEY')
 OWNER = environ.get('OWNER','BotDunia')
-USERS = environ.get('USERS').split()):
+USERS = [int(i) for i in environ.get("AUTH_USERS", "1478357602").split(" ")]
