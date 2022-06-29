9885a8b44704dc05fc2b7e89e0ca5e0be08fb614
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-29 19:55:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,12 +14,16 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(leve
 log = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+import os
 from os import environ
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyshorteners import Shortener
-from sample_config import Config
-from config import Config
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
 
 SHORTLINKBOT = Client('ShortlinkBot',
              api_id=API_ID,
