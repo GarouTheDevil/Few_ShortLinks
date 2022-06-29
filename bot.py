84035c3e25414abc584e931058454bb18c1caa41
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-29 18:30:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ from pyshorteners import Shortener
 from config import *
 from sample_config import Config
 
+BITLY_KEY = "96bbcd78781ecd9018670be6c876452af049084a"
 API_ID=10293305
 API_HASH = "9b3bb6ac48fbbc1195e4657fe30858e9"
 BOT_TOKEN = "5519560955:AAHkEKKpNMIoptfSbk9vV5q-Wh5XTOmg8n4"
