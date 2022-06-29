e8ea49f11ab191483acc125b4e33a38902f094a9
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-29 18:16:44 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,4 @@
 from os import environ
 
 class Config(object):
-USERS = [int(i) for i in environ.get("USERS", "").split(" ")]
-
+USERS = [int(i) for i in environ.get("USERS", "1802569102 1016768333").split(" ")]
