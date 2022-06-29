8029c7447daa37a9bf3e1ae928559fa4f1d9e0ba
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-29 19:32:20 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
 from os import environ
 
 class Config(object):
-API_ID = int(environ.get("API_ID", 10293305))
+API_ID = 10293305
 API_HASH = (environ.get("API_HASH", "9b3bb6ac48fbbc1195e4657fe30858e9"))
 BOT_TOKEN = (environ.get("BOT_TOKEN", "5519560955:AAHkEKKpNMIoptfSbk9vV5q-Wh5XTOmg8n4"))
 BITLY_KEY = (environ.get("96bbcd78781ecd9018670be6c876452af049084a"))
