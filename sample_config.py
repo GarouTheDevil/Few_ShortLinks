e3b125cb92fc30dc4579e517991b10d70515aed4
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-29 21:07:28 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,6 +4,6 @@ class Config(object):
  API_ID = int(environ.get("API_ID")
  API_HASH = int(environ.get("API_HASH", ""))
  BOT_TOKEN = int(environ.get("BOT_TOKEN", ""))
- BITLY_KEY = int(environ.get("BITLY_KEY, ""))
+ BITLY_KEY = int(environ.get("BITLY_KEY", ""))
  OWNER = int(environ.get("OWNER", ""))
  USERS = [int(i) for i in environ.get("USERS", "").split(" ")]
